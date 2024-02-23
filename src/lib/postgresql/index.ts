8ed6106ab63e570e3a13c64956db0a324f9714c3
acomarcho <acoxstpd@gmail.com>
2024-02-23 10:26:13 +0700

    Feat: Implement data access for user

---
+++ src/lib/postgresql/index.ts
@@ -0,0 +1,19 @@
+import { Pool } from "pg";
+
+class PostgreSqlDatabase {
+  private pool: Pool;
+
+  constructor() {
+    this.pool = new Pool();
+  }
+
+  async getPool() {
+    return this.pool;
+  }
+
+  async getClient() {
+    return await this.pool.connect();
+  }
+}
+
+export const postgreSqlDatabase = new PostgreSqlDatabase();
---
+++ src/user/entity/index.ts
@@ -0,0 +1,41 @@
+import { v4 as uuidv4 } from "uuid";
+
+export class UserEntity {
+  private id;
+  private username;
+  private email;
+  private hashedPassword;
+
+  constructor({
+    id = uuidv4(),
+    username,
+    email,
+    hashedPassword,
+  }: {
+    id?: string;
+    username: string;
+    email: string;
+    hashedPassword: string;
+  }) {
+    this.id = id;
+    this.username = username;
+    this.email = email;
+    this.hashedPassword = hashedPassword;
+  }
+
+  public getId() {
+    return this.id;
+  }
+
+  public getUsername() {
+    return this.username;
+  }
+
+  public getEmail() {
+    return this.email;
+  }
+
+  public getHashedPassword() {
+    return this.hashedPassword;
+  }
+}
