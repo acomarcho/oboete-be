a0af905fae4479c60bb0e8760911fcaaedda4abd
acomarcho <acoxstpd@gmail.com>
2024-02-25 10:19:35 +0700

    Feat: Initialize app

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
+import { PostgreSqlUserDataAccess } from "./user/data-access/postgresql";
+import { RegisterUseCase } from "./user/use-case/register";
+import { UserController } from "./user/controller";
 
 dotenv.config();
 
 const app: Express = express();
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+const postgreSqlUserDataAccess = new PostgreSqlUserDataAccess();
+const registerUseCase = new RegisterUseCase(postgreSqlUserDataAccess);
+const userController = new UserController({
+  registerUseCase,
 });
+app.use("/user", userController.getRouter());
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
--- src/user/data-access/postgresql/index.ts
+++ src/user/data-access/postgresql/index.ts
@@ -29,7 +29,7 @@ export class PostgreSqlUserDataAccess implements UserDataAccessInterface {
     } catch {
       await client.query("ROLLBACK");
     } finally {
-      await client.release();
+      client.release();
     }
 
     return user;
