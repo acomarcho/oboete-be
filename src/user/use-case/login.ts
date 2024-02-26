e20729f8e4947fc3bc392aa580dc714086ddbec0
acomarcho <acoxstpd@gmail.com>
2024-02-26 20:22:48 +0700

    Feat: Initialize login use case

---
+++ src/user/use-case/login.ts
@@ -0,0 +1,66 @@
+import { HttpError } from "../../lib/error/http-error";
+import { UserDataAccessInterface } from "../data-access-interface";
+import jwt from "jsonwebtoken";
+import { StatusCodes } from "http-status-codes";
+import bcrypt from "bcrypt";
+
+export class LoginUseCase {
+  private userDataAccess: UserDataAccessInterface;
+
+  constructor(userDataAccess: UserDataAccessInterface) {
+    this.userDataAccess = userDataAccess;
+  }
+
+  async execute({
+    usernameOrEmail,
+    password,
+  }: {
+    usernameOrEmail: string;
+    password: string;
+  }) {
+    const userByEmail = await this.userDataAccess.getUserByEmail(
+      usernameOrEmail
+    );
+    const userByUsername = await this.userDataAccess.getUserByUsername(
+      usernameOrEmail
+    );
+    const user = userByEmail || userByUsername;
+
+    if (user === null) {
+      throw new HttpError(StatusCodes.BAD_REQUEST, "Invalid credentials!");
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user.getHashedPassword()
+    );
+
+    if (!isPasswordCorrect) {
+      throw new HttpError(StatusCodes.BAD_REQUEST, "Invalid credentials!");
+    }
+
+    const refreshToken = await jwt.sign(
+      {
+        userId: user.getId(),
+      },
+      process.env.REFRESH_SECRET || "secret",
+      {
+        expiresIn: "30d",
+      }
+    );
+    const accessToken = await jwt.sign(
+      {
+        userId: user.getId(),
+      },
+      process.env.ACCESS_SECRET || "secret",
+      {
+        expiresIn: "5m",
+      }
+    );
+
+    return {
+      refreshToken,
+      accessToken,
+    };
+  }
+}
