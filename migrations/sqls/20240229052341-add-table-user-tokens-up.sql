c456d939cff68521049eb974d5c9200d26f8649c
acomarcho <acoxstpd@gmail.com>
2024-02-29 19:29:31 +0700

    Fix: Add primary key in migration

--- migrations/sqls/20240229052341-add-table-user-tokens-up.sql
+++ migrations/sqls/20240229052341-add-table-user-tokens-up.sql
@@ -1,5 +1,5 @@
 create table user_tokens(
-  id uuid default gen_random_uuid(),
+  id uuid default gen_random_uuid() primary key,
   user_id uuid not null references users(id),
   token varchar not null
 );
